67f431bd5085226f19039786a36d00eff3e6a180
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-07 21:41:44 -0300

    Trabalhando com data annotations

--- TesteMVC5/Models/Aluno.cs
+++ TesteMVC5/Models/Aluno.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TesteMVC5.Models
 {
     public class Aluno
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
         public string Nome { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido")]
         public string CPF { get; set; }
-        public DateTime Datamatricula { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
         public bool Ativo { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas informadas não conferem")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string SenhaConfirmacao { get; set; }
     }
 }

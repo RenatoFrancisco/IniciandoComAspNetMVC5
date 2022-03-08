5a2fc544736d0ff643e2fa21e7a037475f1253ee
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-07 21:32:59 -0300

    O que é uma model

---
+++ TesteMVC5/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TesteMVC5.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime Datamatricula { get; set; }
+        public bool Ativo { get; set; }
+    }
+}

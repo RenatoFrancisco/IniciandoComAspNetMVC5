dd3f40e14c5d8e988d251937d97ccba4c3fe5234
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-07 22:00:44 -0300

    Validação de modelos

---
+++ TesteMVC5/Controllers/AlunoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using TesteMVC5.Models;
+
+namespace TesteMVC5.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [Route("novo-aluno")]
+        public ActionResult Novo(Aluno aluno)
+        {
+            aluno = new Aluno
+            {
+                Id = 1,
+                Nome = "Renato",
+                CPF = "12345678900",
+                DataMatricula = DateTime.Now,
+                Email = "renato@teste.com",
+                Ativo = true
+            };
+
+            return RedirectToAction("Index", aluno);
+        }
+
+        public ActionResult Index(Aluno aluno)
+        {
+            if (!ModelState.IsValid) return View(aluno);
+            return View(aluno);
+        }
+    }
+}

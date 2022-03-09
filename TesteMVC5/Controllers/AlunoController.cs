e7ebe2853f99b9c9f0084955ad17c3976c7b8d4b
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-08 22:20:27 -0300

    Trabalhando com HtmlHelpers

--- TesteMVC5/Controllers/AlunoController.cs
+++ TesteMVC5/Controllers/AlunoController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using TesteMVC5.Models;
 
 namespace TesteMVC5.Controllers
@@ -7,24 +6,10 @@ namespace TesteMVC5.Controllers
     public class AlunoController : Controller
     {
         [Route("novo-aluno")]
-        public ActionResult Novo(Aluno aluno)
-        {
-            aluno = new Aluno
-            {
-                Id = 1,
-                Nome = "Renato",
-                CPF = "12345678900",
-                DataMatricula = DateTime.Now,
-                Email = "renato@teste.com",
-                Ativo = true
-            };
-
-            return RedirectToAction("Index", aluno);
-        }
-
-        public ActionResult Index(Aluno aluno)
+        public ActionResult NovoAluno(Aluno aluno)
         {
             if (!ModelState.IsValid) return View(aluno);
+
             return View(aluno);
         }
     }

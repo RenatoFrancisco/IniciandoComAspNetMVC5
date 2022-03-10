af70a2b5e06f46c10e6697cc81016a02cea5839c
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-09 22:38:44 -0300

    Proteção contra CSRF

--- TesteMVC5/Controllers/AlunoController.cs
+++ TesteMVC5/Controllers/AlunoController.cs
@@ -13,6 +13,7 @@ namespace TesteMVC5.Controllers
         }
 
         [HttpPost()]
+        [ValidateAntiForgeryToken]
         [Route("novo-aluno")]
         public ActionResult NovoAluno(Aluno aluno)
         {

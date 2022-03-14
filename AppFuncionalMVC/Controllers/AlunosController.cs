996977a8552c4b5ee19c871864e6fc01453b92ed
renatofrancisco <renatofrancisco.s@gmail.com>
2022-03-14 13:02:06 -0300

    Customizando a implementação padrão

--- AppFuncionalMVC/Controllers/AlunosController.cs
+++ AppFuncionalMVC/Controllers/AlunosController.cs
@@ -1,127 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using AppFuncionalMVC.Models;
 using System.Data.Entity;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using AppFuncionalMVC.Models;
 
 namespace AppFuncionalMVC.Controllers
 {
     public class AlunosController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
 
-        // GET: Alunos
+        [HttpGet]
+        [Route("listar-alunos")]
         public async Task<ActionResult> Index()
         {
-            return View(await db.Alunos.ToListAsync());
+            return View(await _context.Alunos.ToListAsync());
         }
 
-        // GET: Alunos/Details/5
-        public async Task<ActionResult> Details(int? id)
+        [HttpGet]
+        [Route("aluno-detalhe/{id:int}")]
+        public async Task<ActionResult> Details(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Aluno aluno = await db.Alunos.FindAsync(id);
-            if (aluno == null)
-            {
-                return HttpNotFound();
-            }
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null) return HttpNotFound();
+
             return View(aluno);
         }
 
-        // GET: Alunos/Create
+        [HttpGet]
+        [Route("novo-aluno")]
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Alunos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("novo-aluno")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Email,CPF,DataMatricula,Ativo")] Aluno aluno)
         {
             if (ModelState.IsValid)
             {
-                db.Alunos.Add(aluno);
-                await db.SaveChangesAsync();
+                _context.Alunos.Add(aluno);
+                await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
             return View(aluno);
         }
 
-        // GET: Alunos/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        [HttpGet]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Aluno aluno = await db.Alunos.FindAsync(id);
-            if (aluno == null)
-            {
-                return HttpNotFound();
-            }
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null) return HttpNotFound();
+
             return View(aluno);
         }
 
-        // POST: Alunos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Route("editar-aluno/{id:int}")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Email,CPF,DataMatricula,Ativo")] Aluno aluno)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(aluno).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                _context.Entry(aluno).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
+
             return View(aluno);
         }
 
-        // GET: Alunos/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        [HttpGet]
+        [Route("excluir-aluno/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Aluno aluno = await db.Alunos.FindAsync(id);
-            if (aluno == null)
-            {
-                return HttpNotFound();
-            }
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null) return HttpNotFound();
+
             return View(aluno);
         }
 
-        // POST: Alunos/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [Route("excluir-aluno/{id:int}")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Aluno aluno = await db.Alunos.FindAsync(id);
-            db.Alunos.Remove(aluno);
-            await db.SaveChangesAsync();
+            var aluno = await _context.Alunos.FindAsync(id);
+            _context.Alunos.Remove(aluno);
+            await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
-                db.Dispose();
-            }
+                _context.Dispose();
+
             base.Dispose(disposing);
         }
     }
